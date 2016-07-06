d4b573887d24a73de6396ae89d1572d6f18d61eb
Krunal Patel <krunal.patel@starbreeze.com>
2016-07-06 16:14:02 -0700

    more in line with how other scripts work: push into the depdency direcotyr, do your stuff, and popout

--- build.sh
+++ build.sh
@@ -1,10 +1,11 @@
-pushd curl
+pushd "$(dirname "$0")"/curl
+
 echo "About to build libCurl"
 pwd
 export MACOSX_DEPLOYMENT_TARGET="10.10"
 ./configure --disable-ldap --without-ssl --without-libidn  --without-libssh2
 make -j8
-popd
-
 # FIX for Curl on Mac - we want to ignore changes to the tracked files below
-git update-index --skip-worktree -- curl/Makefile curl/include/curl/curlbuild.h
+git update-index --skip-worktree -- Makefile include/curl/curlbuild.h
+
+popd
