7998043cccfdc48cf966250b286a159758dc52b0
Krunal Patel <krunal.patel@starbreeze.com>
2016-07-06 15:09:13 -0700

    add build scripts

---
+++ build.bat
@@ -0,0 +1,22 @@
+@echo off
+
+set TOOLCHAIN=%1
+set OLDPATH=%path%
+
+if %TOOLCHAIN% equ msvc2013 (
+    call "%VS120COMNTOOLS%../../VC/bin/amd64/vcvars64.bat"
+    set VCVALUE=12
+) else if %TOOLCHAIN% equ msvc2015 (
+    call "%VS140COMNTOOLS%../../VC/bin/amd64/vcvars64.bat"
+    set VCVALUE=14
+) else (
+    echo Failure: unknown msvc version %TOOLCHAIN%
+    exit /b /1
+)
+
+cd curl
+cd winbuild
+call nmake Makefile.vc mode=static MACHINE=x64 VC=%VCVALUE% ENABLE_WINSSL=no
+
+set PATH=%OLDPATH%
+
---
+++ build.sh
@@ -0,0 +1,6 @@
+cd curl
+echo "About to build libCurl"
+pwd
+export MACOSX_DEPLOYMENT_TARGET="10.10"
+./configure --disable-ldap --without-ssl --without-libidn  --without-libssh2
+make -j8
