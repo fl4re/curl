7e905d8a108ee8bcbd19bb376136bc91214bff28
Krunal Patel <krunal.patel@starbreeze.com>
2016-07-06 16:05:52 -0700

    reorg

--- build.sh
+++ build.sh
@@ -5,3 +5,6 @@ export MACOSX_DEPLOYMENT_TARGET="10.10"
 ./configure --disable-ldap --without-ssl --without-libidn  --without-libssh2
 make -j8
 popd
+
+# FIX for Curl on Mac - we want to ignore changes to the tracked files below
+git update-index --skip-worktree -- curl/Makefile curl/include/curl/curlbuild.h
