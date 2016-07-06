3ca721e3c3525ca387f166c4d9069435df246470
Krunal Patel <krunal.patel@starbreeze.com>
2016-07-06 15:35:23 -0700

    pushing & popping dirs

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
-cd curl
+pushd curl
 echo "About to build libCurl"
 pwd
 export MACOSX_DEPLOYMENT_TARGET="10.10"
 ./configure --disable-ldap --without-ssl --without-libidn  --without-libssh2
 make -j8
+popd
